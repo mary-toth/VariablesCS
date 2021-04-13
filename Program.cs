d3a89f616b5fbc6f4c586da84b3c5e293640a6fb
Mary Toth <maryarylynn@gmail.com>
2021-04-13 12:49:32 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using VariablesCS.Models;
+using System.Linq;
+
+namespace VariablesCS
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Welcome to C#");
+
+    }
+  }
+}
